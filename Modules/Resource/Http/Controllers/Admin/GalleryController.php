0f9d7333850f9279d30b1594959fa82f6ed01a2b
Justin <cong.itsoft@gmail.com>
2017-01-25 13:53:13 +0700

    admin user edit

--- Modules/Resource/Http/Controllers/Admin/GalleryController.php
+++ Modules/Resource/Http/Controllers/Admin/GalleryController.php
@@ -2,8 +2,25 @@
 
 namespace Modules\Resource\Http\Controllers\Admin;
 
+use Modules\Resource\Repositories\ResourceRepository;
 use Nht\Http\Controllers\Admin\AdminController;
 
 class GalleryController extends AdminController {
 
+    /**
+     * [$resource description]
+     * @var \Modules\Resource\Repositories\ResourceRepository
+     */
+    protected $resource;
+
+    public function __construct(ResourceRepository $resource)
+    {
+        parent::__construct();
+        $this->resource = $resource;
+    }
+
+    public function getIndex(Request $request)
+    {
+        # code...
+    }
 }
