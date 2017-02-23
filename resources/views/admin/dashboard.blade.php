06d00189a4afa532a42b4ec796c23bedbfa7beb2
Justin <cong.itsoft@gmail.com>
2017-02-23 17:12:08 +0700

    active dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,7 +12,7 @@
 				$btnClassArray = ['btn-primary', 'btn-success', 'btn-info', 'btn-danger', 'btn-warning', 'btn-default'];
 			?>
 			@foreach(admin_sidebar() as $item)
-                @if(isset($item['permission']) && Entrust::can($item['permission']) || Entrust::hasRole('root'))
+                @if(isset($item['permission']) && Entrust::can($item['permission']) && array_get($item, 'active') == 1 || Entrust::hasRole('root'))
                 <?php
 					$btnClass = $btnClassArray[array_rand($btnClassArray)];
 				?>
