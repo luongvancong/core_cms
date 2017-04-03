9a284e2b4da53020728536bef7c292c7f363c96c
Justin <cong.itsoft@gmail.com>
2017-04-03 15:22:55 +0700

    price filter product

--- Modules/Post/Http/Controllers/Frontend/PostController.php
+++ Modules/Post/Http/Controllers/Frontend/PostController.php
@@ -8,14 +8,20 @@ use Nht\Http\Controllers\FrontendController;
 
 class PostController extends FrontendController
 {
-    public function __construct(PostRepository $post)
+    public function __construct(PostRepository $post, PostCategoryRepository $postCategory)
     {
         parent::__construct();
         $this->post = $post;
+        $this->postCategory = $postCategory;
     }
 
     public function getIndex(Request $request)
     {
         return view('post::frontend/index');
+    }
+
+    public function getDetail($id, $slug)
+    {
+        return view('post::frontend/detail');
     }
 }
