43d54765e4671cc11350da59574e4e8d7474d9a2
Justin <cong.itsoft@gmail.com>
2017-08-29 14:10:04 +0700

    update core, extract setting to multi row

--- Modules/Setting/Repositories/DbSettingRepository.php
+++ Modules/Setting/Repositories/DbSettingRepository.php
@@ -13,8 +13,14 @@ use App\Hocs\Core\BaseRepository;
 class DbSettingRepository extends BaseRepository implements SettingRepository
 {
 	protected $model;
+
 	public function __construct(Setting $model)
 	{
 		$this->model = $model;
 	}
+
+    public function getAllActive()
+    {
+        return $this->model->where('active', 1)->get();
+    }
 }
