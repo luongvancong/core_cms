9a3074a738c76928ea0466a08a68ef647fe074c1
Justin <cong.itsoft@gmail.com>
2017-09-22 11:07:37 +0700

    config metadata

---
+++ config/metadata.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'image_width' => 200,
+	'image_height' => 200,
+	'fb_admins' => 100001247771720,
+	'fb_app_id' => 817495551734811
+];
--- resources/views/frontend/default.blade.php
+++ resources/views/frontend/default.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <title> {{ setting()->meta_title }} </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta http-equiv="content-language" content="vi" />
+    <link rel="alternate" href="{{ url()->current() }}" hreflang="vi-vn" />
 
     <meta name="keywords" content="{{ setting()->meta_keyword }}">
     <meta name="description" content="{{ setting()->meta_description }}">
@@ -13,19 +14,25 @@
     <!-- Schema.org markup for Google+ -->
     <meta itemprop="name" content="{{ setting()->meta_title }}">
     <meta itemprop="description" content="{{ setting()->meta_description }}">
-    <meta itemprop="image" content="{{ setting()->image }}">
+    <meta itemprop="image" content="{{ setting()->image ? setting()->image : (setting()->meta_image ? url(parse_image_url(setting()->meta_image)) : "" ) }}">
 
     <!-- Open Graph data -->
     <meta property="og:title" content="{{ setting()->meta_title }}" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:image" content="{{ setting()->image }}" />
-    <meta property="og:image:width" content="200">
-    <meta property="og:image:height" content="200">
+    @if(setting()->image instanceof Illuminate\Support\Collection || is_array(setting()->image))
+        @foreach(setting()->image as $item)
+            <meta property="og:image" content="{{ $item }}" />
+        @endforeach
+    @else
+        <meta property="og:image" content="{{ setting()->image ? setting()->image : (setting()->meta_image ? url(parse_image_url(setting()->meta_image)) : "" ) }}" />
+    @endif
+    <meta property="og:image:width" content="{{ config('metadata.image_width') }}">
+    <meta property="og:image:height" content="{{ config('metadata.image_height') }}">
     <meta property="og:description" content="{{ setting()->meta_description }}" />
     <meta property="og:site_name" content="{{ $_SERVER['SERVER_NAME'] }}" />
-    <meta property="fb:admins" content="100001247771720" />
-    <meta property="fb:app_id" content="442754092739471" />
+    <meta property="fb:admins" content="{{ config('metadata.fb_admins') }}" />
+    <meta property="fb:app_id" content="{{ config('metadata.fb_app_id') }}" />
 
     @yield('styles')
 </head>
